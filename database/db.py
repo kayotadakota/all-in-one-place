e9181f4d0e08619b796fbf08b4e11067d744d462
kayotadakota <kayota1992@yandex.ru>
2025-04-17 17:15:07 +0300

    implemented fetch_cmoa_results function

--- database/db.py
+++ database/db.py
@@ -1,6 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
+
+from database.models import Title
 
 
 engine = create_engine('sqlite:///titles.db', echo=True)
 Session = sessionmaker(engine)
+
+
+def fetch_cmoa_results() -> list[tuple]:
+    titles = []
+    with Session.begin() as session:
+        titles.extend(session.execute(select(Title)).all())
+        session.expunge_all()
+
+    return titles
