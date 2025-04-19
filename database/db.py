0ec81a3d7a94ca409f19d568e82696327f3c4c3d
kayotadakota <kayota1992@yandex.ru>
2025-04-19 12:09:16 +0300

    implement insertion method

--- database/db.py
+++ database/db.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, select
+from datetime import date
+
+from sqlalchemy import create_engine, select, insert
 from sqlalchemy.orm import sessionmaker
 
 from database.models import Title
@@ -22,5 +24,20 @@ def delete_title(id: str) -> bool:
         title = session.get(Title, id)
         if title:
             session.delete(title)
+            return True
+    return False
+
+
+def add_title(data: dict):
+    with Session.begin() as session:
+        title = Title(
+            id=data['id'],
+            url=data['url'],
+            img_url=data['img_url'],
+            release_date=data['release_date']
+        )
+        exist = session.get(Title, data['id'])
+        if not exist:
+            session.add(title)
             return True
     return False
